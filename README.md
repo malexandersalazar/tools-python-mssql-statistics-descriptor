541fa80d9eba912a361d320de523e908a319b94f
Alexander Salazar <malex_sv92@hotmail.com>
2023-05-12 18:15:55 -0500

    init repo

--- README.md
+++ README.md
@@ -1,2 +1,89 @@
-# tools-python-mssql-statistics-descriptor
- 
+# MSSQL Statistics Descriptor
+
+![alt text](/img/viz.png "Snowflake Statistics Descriptor")
+
+A lightweight tool based on sweetviz that generates high-density visualizations to kickstart Exploratory Data Analysis within Snowflake with just one line of code.
+
+## Installation
+
+Copy the `main.py` script and install the requirements located in the dist folder.
+
+```
+pip install -r requirements.txt
+```
+
+We will also need to download and install the ODBC Driver for SQL Server, this repo is using the ODBC Driver 18 for SQL Server version.
+
+[Download ODBC Driver for SQL Server](https://learn.microsoft.com/en-us/sql/connect/odbc/download-odbc-driver-for-sql-server)
+
+## Getting Started
+
+| Positional arguments | Example |
+| --- | --- |
+| server | tcp:my-sqldbs-dev.database.windows.net |
+| database | sqldb-adventureworkslt-dev |
+| schema | MySchema |
+
+| Optional arguments | Example/Description |
+| --- | --- |
+| -h, --help | show this help message and exit |
+| -u, --user | database-user, server-admin@contoso.com |
+| -p, --password | specifies the user password, required only at non-interactive login' |
+| -r, --rows | specifies the number of rows to sample from the table (default: 500000) |
+| -l, --level | specifies the database object level in which the analysis should be executed, "s" for schema and "t" for table (default: "s") |
+| -t, --table | specifies the database table name |
+| --associations | indicates that a correlation graph should be generated |
+| --open-browser | indicates that a web browser tab should be opened while datasets are analyzed |
+| --interactive | indicates that program should authenticate with an Azure Active Directory identity using interactive authentication, requires Azure Active Directory admin enabled on Azure SQL server resource |
+
+The default behaviour of the script will load and analyze the specified number of rows of each table in the selected database schema.
+
+```
+python main.py tcp:my-sqldbs-dev.database.windows.net sqldb-adventureworkslt-dev SalesLT -u=database-user -p=S3cUr3P@S$w0rD -r=10000
+```
+
+The program will build and save locally high-density HTML visualizations and generate an Excel summary with table name, table rows, data size, table index size and parsed record count in a new folder called **obj**.
+
+![alt text](/img/cmd.png "Azure SQL Database Statistics Descriptor")
+
+If we need a correlation graph to be generated for the columns of each table, we must include the `--associations` flag.
+
+```
+python main.py tcp:my-sqldbs-dev.database.windows.net sqldb-adventureworkslt-dev SalesLT -u=database-user -p=S3cUr3P@S$w0rD -r=10000--associations
+```
+
+We must consider that correlations and other associations may take a **quadratic time (n^2)** to complete.
+
+![alt text](/img/associations.png "Azure SQL Database Statistics Descriptor")
+
+If we only need the analysis for a single table we must specify "**t**" as `-l` or `--level` argument value with the corresponding **table name** in `-t` or `--table` argument.
+
+```
+python main.py tcp:my-sqldbs-dev.database.windows.net sqldb-adventureworkslt-dev SalesLT -u=database-user -p=S3cUr3P@S$w0rD -r=500000 -l=t -t=Product
+```
+
+If we need an Azure Active Directory authentication we have to set the `--interactive` flag and enable Azure Active Directory admin for our database.
+
+```
+python main.py tcp:my-sqldbs-dev.database.windows.net sqldb-adventureworkslt-dev SalesLT -l=t -t=Product -r=10000 --open-browser --interactive
+```
+
+![alt text](/img/aad.png "Azure SQL Database Statistics Descriptor")
+
+## Prerequisites
+
+Snowflake Statistics Descriptor was tested with:
+
+* Python: 3.7.16
+* Packages:
+    * pyodbc: 4.0.39
+    * pandas: 1.3.5
+    * sweetviz: 2.1.4
+    * XlsxWriter: 3.1.0 
+* Anaconda: 2.4.0
+
+## License
+
+This project is licenced under the [MIT License][1].
+
+[1]: https://opensource.org/licenses/mit-license.html "The MIT License | Open Source Initiative"
---
+++ dist/main.py
@@ -0,0 +1,112 @@
+import argparse
+
+parser=argparse.ArgumentParser(
+    description='A lightweight tool based on sweetviz that generates high-density visualizations to kickstart Exploratory Data Analysis within Microsoft Azure SQL Database with just one line of code.',
+    epilog='github.com/malexandersalazar/tools-python-mssql-statistics-descriptor')
+parser.add_argument('server', type=str, help='tcp:my-db-server.database.windows.net')
+parser.add_argument('database', type=str, help='sqldb-adventureworkslt-dev')
+parser.add_argument('schema', type=str, help='MySchema')
+parser.add_argument('-u','--user', type=str, help='database-user, server-admin@contoso.com')
+parser.add_argument('-p','--password', type=str, help='specifies the user password, required only at non-interactive login')
+parser.add_argument('-r','--rows', default=500000, type=int, help='specifies the number of rows to sample from the table (default: 500000)')
+parser.add_argument('-l','--level', choices=['s','t'], default='s', type=str, help='specifies the database object level in which the analysis should be executed, "s" for schema and "t" for table (default: "s")')
+parser.add_argument('-t','--table', type=str, help='specifies the database table name')
+parser.add_argument('--associations', dest='associations', action='store_true', help='indicates that a correlation graph should be generated')
+parser.set_defaults(associations=False)
+parser.add_argument('--open-browser', dest='open_browser', action='store_true', help='indicates that a web browser tab should be opened while datasets are analyzed')
+parser.set_defaults(open_browser=False)
+parser.add_argument('--interactive', dest='interactive', action='store_true', help='indicates that program should authenticate with an Azure Active Directory identity using interactive authentication, requires Azure Active Directory admin enabled on Azure SQL server resource')
+parser.set_defaults(interactive=False)
+args=parser.parse_args()
+
+SERVER = args.server
+DATABASE = args.database
+SCHEMA = args.schema
+UID = args.user
+PASSWORD = args.password
+DRIVER = '{ODBC Driver 18 for SQL Server}'
+ROWS = args.rows
+LEVEL = args.level
+TABLE = args.table
+ASSOCIATIONS = args.associations
+OPEN_BROWSER = args.open_browser
+INTERACTIVE = args.interactive
+
+import os
+import pyodbc
+import pandas as pd
+import sweetviz as sv
+import gc
+
+def create_connection():
+    if INTERACTIVE:
+        return pyodbc.connect('DRIVER='+DRIVER+';SERVER='+SERVER+';PORT=1433;DATABASE='+DATABASE+';AUTHENTICATION=ActiveDirectoryInteractive')
+    else:
+        return pyodbc.connect('DRIVER='+DRIVER+';SERVER='+SERVER+';PORT=1433;DATABASE='+DATABASE+';UID='+UID+';PWD='+ PASSWORD)
+
+tables = []
+
+if(LEVEL=='s'):
+    with create_connection() as conn1:
+        with conn1.cursor() as schema_cur:
+            schema_cur.execute(f"SELECT TABLE_NAME FROM [{DATABASE}].INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE' AND TABLE_SCHEMA='{SCHEMA}'")
+            schema_row = schema_cur.fetchone()
+            while schema_row:
+                table_name = str(schema_row[0]) 
+                with create_connection() as conn2:
+                    with conn2.cursor() as space_cur:
+                        space_cur.execute(f"EXEC sp_spaceused N'{SCHEMA}.{table_name}'") # result columns: name rows reserved data index_size unused
+                        space_row = space_cur.fetchone()
+                        rows = str(space_row[1])
+                        data = str(space_row[3]) 
+                        index_size = str(space_row[4]) 
+                        tables.append((f'{SCHEMA}.{table_name}',rows,data,index_size))
+                schema_row = schema_cur.fetchone()
+else:
+    with create_connection() as conn1:
+        with create_connection() as conn2:
+            with conn2.cursor() as space_cur:
+                space_cur.execute(f"EXEC sp_spaceused N'{SCHEMA}.{TABLE}'") # result columns: name rows reserved data index_size unused
+                space_row = space_cur.fetchone()
+                rows = str(space_row[1])
+                data = str(space_row[3]) 
+                index_size = str(space_row[4]) 
+                tables.append((f'{SCHEMA}.{TABLE}',rows,data,index_size))
+
+if not os.path.exists('obj'):
+    os.mkdir('obj')
+
+info_arr = []
+
+for (table_name,rows,data,index_size) in tables:
+    print(f'Loading {table_name}...')
+
+    sheet_name = table_name.split('.')[-1]
+
+    sample_query = f'SELECT * FROM {table_name} TABLESAMPLE ({ROWS} ROWS)'
+    with create_connection() as conn3:
+        sample_cur = conn3.cursor().execute(sample_query)
+        sample_df = pd.DataFrame.from_records(
+            iter(sample_cur), columns=[x[0] for x in sample_cur.description])
+
+        info_arr.append([table_name,rows,data,index_size, len(sample_df)])
+
+        analysis = sv.analyze(sample_df, pairwise_analysis=("on" if ASSOCIATIONS else "off"))
+        analysis.show_html(f'obj/{sheet_name}.html', open_browser=OPEN_BROWSER)
+
+    del sample_df
+    gc.collect()
+
+# Writing summary
+eda_info_df = pd.DataFrame(info_arr, columns = ['NAME','TABLE ROWS','DATA','INDEX SIZE','SAMPLE ROWS'])
+excel_writer = pd.ExcelWriter(f'obj/{SCHEMA}_EDA_INFO.xlsx', engine='xlsxwriter')
+eda_info_df.to_excel(excel_writer, sheet_name=SCHEMA, index=False)
+worksheet = excel_writer.sheets[SCHEMA]
+for idx, col in enumerate(eda_info_df):  # Loop through all columns
+    series = eda_info_df[col]
+    max_len = max((
+        series.astype(str).str.len().max(),  # Len of largest item
+        len(str(series.name))  # Len of column name/header
+        )) + 9  # Adding a little extra space
+    worksheet.set_column(idx, idx, max_len)  # Set column width
+excel_writer.close()
